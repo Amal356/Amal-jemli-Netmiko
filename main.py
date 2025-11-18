f5ebfa21ea0791d73ce458f9ced3554f1d8c42a2
amal <amalyousef356@gmail.com>
2025-11-18 09:04:20 +0100

    Ajout de la fonction acces_netmiko

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+def acces_netmiko():
+    from netmiko import ConnectHandler
+    device = {
+        "device_type": "cisco_xr",
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+        "port": 22,
+    }
+    with ConnectHandler(**device) as conn:
+        print(conn.send_command("show clock"))
+        interfaces = conn.send_command("show interfaces")
+        with open("interfaces.txt", "w", encoding="utf-8") as f:
+            f.write(interfaces)
+
+def dire_bonjour():
+    print("Bonjour, Git!")
+
 print("Hello, Git!")
+dire_bonjour()
